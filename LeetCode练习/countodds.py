69ffdbe710937f0bfe4ea0a43d3026c9e0699609
Tom  Huang <76953420+GoodBoyHuang@users.noreply.github.com>
2021-01-05 18:49:25 +0800

    Create countodds.py
    在给区间内计算奇数的个数

---
+++ LeetCode练习/countodds.py
@@ -0,0 +1,16 @@
+#在区间范围内统计奇数数目
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        result = []
+        for x in range(low,high+1):
+            if x%2 == 0:
+                continue
+            else :
+                result.append(x)
+        return len(result)
+
+low = int(input('please enter low value:'))
+high = int(input('please enter high value:'))
+count = Solution()
+num_odd_count = count.countOdds(low,high)
+print("count odds is: ",num_odd_count)
