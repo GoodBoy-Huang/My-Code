629e9f9a17e7f5389add9bb37b93c9af177bc893
Tom  Huang <76953420+GoodBoyHuang@users.noreply.github.com>
2021-01-06 01:03:09 +0800

    Update Break.py

--- 未分类文件/Break.py
+++ 未分类文件/Break.py
@@ -1,1 +1,12 @@
-'''break语句,和C中的类似用于跳出最近的for或while循环.   循环语句可能带有一个else子句；它会在循环遍历完列表(使用for)   或是在条件变为假(使用while)的时候被执行,但是不会在循环被break语句终止时被执行'''for n in range(2, 10):    for x in range(2, n):        if n % x == 0:            print(n, 'equals', x, '*', n//x)            break    else:    # loop fell through without finding a factor        print(n, 'is a prime number')
+'''break语句,和C中的类似用于跳出最近的for或while循环.循环语句可能带有一个else子句；
+   它会在循环遍历完列表(使用for) 或是在条件变为假(使用while)的时候被执行,但是不会
+   在循环被break语句终止时被执行'''
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+    # loop fell through without finding a factor
+        print(n, 'is a prime number')
