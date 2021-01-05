041afcc2619dca7ba88b9a7d46c10b374d0abf9e
Tom  Huang <76953420+GoodBoyHuang@users.noreply.github.com>
2021-01-06 01:15:32 +0800

    Create list_code.py

---
+++ 基础语句/list_code.py
@@ -0,0 +1,14 @@
+#用索引取出下面list的指定元素：
+
+name_list = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+    ]
+
+# 打印Apple:
+print(name_list[0][0])
+# 打印Python:
+print(name_list[1][1])
+# 打印Lisa:
+print(name_list[2][2])
