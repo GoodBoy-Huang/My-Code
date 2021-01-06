efedab7aeb14d9da1634ae462968eaaab3618ec8
Tom  Huang <76953420+GoodBoyHuang@users.noreply.github.com>
2021-01-07 00:52:13 +0800

    Update Feibonacci.py

--- LeetCode练习/Feibonacci.py
+++ LeetCode练习/Feibonacci.py
@@ -1,1 +1,14 @@
+#斐波那契数列递归计算f(n)的值
+def fib(n):
+    if n ==0:
+        return 0
+    elif n ==1:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
 
+n = int(input(":"))
+fib(n)
+print(fib(n))
+
+#我不知道在Leetcode怎么回事提交就一直显示fib没有定义
